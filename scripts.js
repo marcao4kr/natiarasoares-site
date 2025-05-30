dd3136a054b977bfe35319e88e0168d0591450d9
marcao.bat <85796288+marcao4kr@users.noreply.github.com>
2025-05-30 16:56:54 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-
-const myObserver = new IntersectionObserver ((entries) => {    
-    console.log(entries)
-
-})
-
-const elements = document.querySelectorAll ('.hidden')
-
-elements.forEach((element) => myObserver.observe(element))
